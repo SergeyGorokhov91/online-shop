e1ccbd4efb407b693bfa2310d00017c3220617fe
Сергей Горохов <s.gorokhov91@gmail.com>
2023-10-26 17:04:59 +0300

    Initial commit

---
+++ backend/online-shop-backed/src/main/java/com/example/onlineshopbacked/OnlineShopBackedApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopbacked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopBackedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopBackedApplication.class, args);
+	}
+
+}
---
+++ backend/online-shop-backed/src/test/java/com/example/onlineshopbacked/OnlineShopBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopbacked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopBackedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
