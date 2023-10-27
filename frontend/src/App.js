538ef2bd4a2e20b190588c5080452a44077e3f4e
Сергей Горохов <s.gorokhov91@gmail.com>
2023-10-27 12:07:28 +0300

    Add main page carousel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navigation from "./customer/components/navigation/Navigation";
+import MainCarousel from "./customer/components/HomeCarousel/MainCarousel";
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <MainCarousel/>
       <div>
         home
       </div>
