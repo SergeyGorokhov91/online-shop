2dfc3aea05800e8e7eeb952b42ff8b14f4edee79
Сергей Горохов <s.gorokhov91@gmail.com>
2023-10-27 13:36:52 +0300

    Add images to carousel

--- frontend/src/customer/components/HomeCarousel/MainCarousel.jsx
+++ frontend/src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
+import {mainCarouselData} from "./MainCarouselData";
 import 'react-alice-carousel/lib/alice-carousel.css';
 
-const items = [
-  <div className="item" data-value="1">1</div>,
-  <div className="item" data-value="2">2</div>,
-  <div className="item" data-value="3">3</div>,
-  <div className="item" data-value="4">4</div>,
-  <div className="item" data-value="5">5</div>,
-];
+const MainCarousel = () => {
+  const items = mainCarouselData.map((item) =>
+    <img
+      className="cursor-pointer"
+      src={item.image}
+      role="presentation"
+      alt=""
+    />
+  )
 
-const MainCarousel = () => (
-  <AliceCarousel
-    mouseTracking
-    items={items}
-    controlsStrategy="alternate"
-  />
-);
+  return (
+    <div>
+      <AliceCarousel
+        items={items}
+        disableButtonsControls
+        autoPlay
+        autoPlayInterval={2000}
+        infinite
+      />
+    </div>
+  )
+}
 
 export default MainCarousel;
---
+++ frontend/src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,22 @@
+export const mainCarouselData =[
+  {
+    image: `${process.env.PUBLIC_URL}/images/carousel/woman1.jpg`,
+    alt: 'Image 1',
+    path:"/women/clothing/woman1"
+  },
+  {
+    image: `${process.env.PUBLIC_URL}/images/carousel/woman2.jpg`,
+    alt: 'Image 2',
+    path:"/women/clothing/woman2"
+  },
+  {
+    image: `${process.env.PUBLIC_URL}/images/carousel/woman3.jpg`,
+    alt: 'Image 3',
+    path:"/women/clothing/woman3"
+  },
+  {
+    image: `${process.env.PUBLIC_URL}/images/carousel/woman4.jpg`,
+    alt: 'Image 4' ,
+    path:"/women/clothing/woman4"
+  }
+]
