6c1857294caa918903ce9e8e760f1cd093dcfb22
Сергей Горохов <s.gorokhov91@gmail.com>
2023-10-29 13:43:22 +0300

    Add buttons to items carousel

--- frontend/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ frontend/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import AliceCarousel from "react-alice-carousel";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import {Button} from "@mui/material";
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+
 
 function HomeSectionCarousel() {
   const responsive = {
@@ -15,15 +18,51 @@ function HomeSectionCarousel() {
   const items = [1,1,1,1,1,1,1,1,1,1].map(()=><HomeSectionCard/>)
 
   return (
-    <div className="relative px-4 lg:px-8">
+    <div className="border">
       <div className="relative p-5">
         <AliceCarousel
           items={items}
           disableButtonsControls
           infinite
           responsive={responsive}
-
+          disableDotsControls
         />
+        <Button variant="contained"
+                className="
+                z-50
+                bg-white
+                "
+                sx={{
+                  position:"absolute",
+                  top:"8rem",
+                  right:"0rem",
+                  transform:"translateX(50%) rotate(90deg)",
+                  bgcolor:"white"
+                }}
+                aria-label="next"
+        >
+          <KeyboardArrowLeftIcon sx={{
+            transform:"rotate(90deg)",
+            color:"black"}}/>
+        </Button>
+        <Button variant="contained"
+                className="
+                z-50
+                bg-white
+                "
+                sx={{
+                  position:"absolute",
+                  top:"8rem",
+                  left:"0rem",
+                  transform:"translateX(-50%) rotate(90deg)",
+                  bgcolor:"white"
+                }}
+                aria-label="next"
+        >
+          <KeyboardArrowLeftIcon sx={{
+            transform:"rotate(-90deg)",
+            color:"black"}}/>
+        </Button>
       </div>
     </div>
   );
