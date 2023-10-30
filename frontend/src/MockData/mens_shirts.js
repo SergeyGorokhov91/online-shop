9b1806ed9fff8a33865d45dd10e00ed2973124f6
Сергей Горохов <s.gorokhov91@gmail.com>
2023-10-30 13:51:10 +0300

    add more mock data from open source

--- frontend/src/MockData/mens_shirts.js
+++ frontend/src/MockData/mens_shirts.js
@@ -1,4 +1,4 @@
-export  const  mens_carousel_home_menu = [
+export  const  mens_shirts = [
   {
     "imageUrl": "https://rukminim1.flixcart.com/image/612/612/l5h2xe80/kurta/x/6/n/xl-kast-tile-green-majestic-man-original-imagg4z33hu4kzpv.jpeg?q=70",
     "brand": "Majestic Man",
