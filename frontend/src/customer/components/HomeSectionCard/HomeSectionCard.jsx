cbed1e8a544266504ad7c329d27d4cefb926eb24
Сергей Горохов <s.gorokhov91@gmail.com>
2023-10-30 13:52:36 +0300

    add display data functionality

--- frontend/src/customer/components/HomeSectionCard/HomeSectionCard.jsx
+++ frontend/src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-function HomeSectionCard() {
+function HomeSectionCard({product}) {
   return (
       <div className="border cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3">
         <div className="h-[13rem] w-[10rem]">
           <img className="object-cover object-top w-full h-full"
-               src="https://www.fashionbeans.com/wp-content/uploads/2021/11/Luca_Faloni_Moss-Green-Brushed-Cotton-Shirt_Editorial_Hero_Desktop_Made_in_Italy_2.jpeg" alt=""/>
+               src={product.imageUrl} alt=""/>
         </div>
         <div className="p-4">
-          <h3 className="text-lg font-medium text-gray-900">Nofilter</h3>
-          <p className="mt-2 text-sm text-gray-500">Some text about shirt</p>
+          <h3 className="text-lg font-medium text-gray-900">{product.brand}</h3>
+          <p className="mt-2 text-sm text-gray-500">{product.title}</p>
         </div>
       </div>
   );
--- frontend/src/customer/pages/HomePage/HomePage.jsx
+++ frontend/src/customer/pages/HomePage/HomePage.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import MainCarousel from "../../components/HomeCarousel/MainCarousel";
 import HomeSectionCarousel from "../../components/HomeSectionCarousel/HomeSectionCarousel";
+import {mens_shirts} from "../../../MockData/mens_shirts";
+import {women_dresses} from "../../../MockData/women_dresses";
+import {mens_pants} from "../../../MockData/mens_pants";
+import {women_jeans} from "../../../MockData/women_jeans";
 
 function HomePage() {
   return (
     <div>
       <MainCarousel/>
       <div className="space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10">
-        <HomeSectionCarousel/>
-        <HomeSectionCarousel/>
-        <HomeSectionCarousel/>
-        <HomeSectionCarousel/>
-        <HomeSectionCarousel/>
+        <HomeSectionCarousel data={mens_shirts} sectionName={"Men`s shirts"}/>
+        <HomeSectionCarousel data={mens_pants} sectionName={"Men`s pants"}/>
+        <HomeSectionCarousel data={women_jeans} sectionName={"Women`s jeans"}/>
+        <HomeSectionCarousel data={women_dresses} sectionName={"Women`s dresses"}/>
 
       </div>
     </div>
