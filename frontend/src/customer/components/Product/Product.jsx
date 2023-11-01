aa6598f6a1326f7d7bd7e56ffaa6e9cb6ceadada
Сергей Горохов <s.gorokhov91@gmail.com>
2023-11-01 13:59:55 +0300

    Create product page

---
+++ frontend/src/customer/components/Product/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductCard from "./ProductCard";
+
+function Product(props) {
+  return (
+    <div>
+      <ProductCard/>
+    </div>
+  );
+}
+
+export default Product;
